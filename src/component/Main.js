1f96c24e613da864a21f57f0a6ebceb560b32e57
xtrycatchx <xtrycatchx@gmail.com>
2018-07-07 00:53:52 +0800

    not so lazy approach

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import ThemeProvider from '../theme/themeprovider'
 import Button from './Button'
+import withTheme from '../theme/withTheme';
 
-export default class Main extends Component {
+class Main extends Component {
+
+  // commenting this coz this was the lazy approach :D
+  // for references, so no need to remove
+  // render() {
+  //   return (
+  //     <View style={this.context.theme.rootView}>
+  //       <Text style={this.context.theme.label}>Hello World</Text>
+  //       <Button onPress={this.props.onChangeTheme} label='Change Theme' />
+  //     </View>
+  //   );
+  // }
+
   render() {
     return (
       <View style={this.context.theme.rootView}>
@@ -14,5 +27,23 @@ export default class Main extends Component {
   }
 }
 
-Main.contextTypes = ThemeProvider.childContextTypes;
+const MainComponent = withTheme({
+  theme: {
+    rootView: {
+      flex: PropTypes.number,
+      backgroundColor: PropTypes.string,
+      flexDirection: PropTypes.string,
+      justifyContent: PropTypes.string,
+      alignItems: PropTypes.string,
+    },
+    label: {
+      color: PropTypes.string,
+      fontSize: PropTypes.number,
+      fontStyle: PropTypes.string,
+    },
+  }
+})
+
+// commenting this coz this was the lazy approach :D
+//Main.contextTypes = ThemeProvider.childContextTypes;
 
