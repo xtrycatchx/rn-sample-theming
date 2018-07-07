433edfe0278763e2177cabe44d0bdaec3b175e63
xtrycatchx <xtrycatchx@gmail.com>
2018-07-07 20:47:12 +0800

    refactor provider

--- src/theme/withTheme.js
+++ src/theme/withTheme.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const withTheme = (contextTypes) => (Component) => {
-  ThemedComponent = (props, context) =>
-    <Component {...props} {...context} />;
-  ThemedComponent.contextTypes = contextTypes
-  return ThemedComponent
+// const withTheme = (contextTypes) => (Component) => {
+//   ThemedComponent = (props, context) =>
+//     <Component {...props} {...context} />;
+//   ThemedComponent.contextTypes = contextTypes
+//   return ThemedComponent
+// };
+
+// export default withTheme;
+
+const withTheme = (Child) => class extends React.Component {
+  static contextTypes = {
+    theme: React.PropTypes.object
+  };
+
+  render() {
+    return <Child {...this.props} {...this.context} />;
+  }
 };
 
 export default withTheme;
