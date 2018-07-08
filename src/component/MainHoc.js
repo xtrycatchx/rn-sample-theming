f0825bbdaed490e9fad6a0a3b8ef8dfc7ec7aaa7
xtrycatchx <xtrycatchx@gmail.com>
2018-07-09 00:13:44 +0800

    Update MainHoc.js

--- src/component/MainHoc.js
+++ src/component/MainHoc.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
-import ButtonHoc from './ButtonHoc'
+import ButtonHoc from './ButtonHoc';
 import withThemeHoc from '../theme/withThemeHoc';
 
 const MainHoc = ({ onChangeTheme, theme }) => (
