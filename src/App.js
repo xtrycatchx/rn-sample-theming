e82d9ce3d85ccf47e88f896e1d2655c5d8b9e6e4
xtrycatchx <xtrycatchx@gmail.com>
2018-07-09 00:12:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
-import ThemeProvider from './theme/themeprovider'
-import ThemeProviderHoc from './theme/themeproviderhoc'
-import dayMode from './theme/daymode'
-import nightMode from './theme/nightmode'
-import Main from './component/Main'
-import MainHoc from './component/MainHoc'
+import ThemeProvider from './theme/themeprovider';
+import ThemeProviderHoc from './theme/themeproviderhoc';
+import dayMode from './theme/daymode';
+import nightMode from './theme/nightmode';
+import Main from './component/Main';
+import MainHoc from './component/MainHoc';
 
 export default class App extends Component {
   constructor(props) {
