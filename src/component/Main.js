95a4bc0534b75bb2b7f6eb3c609b44710255c207
Paul Sydney <xtrycatchx@gmail.com>
2018-07-15 22:43:51 +0800

    clean up

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
-//import ThemeProvider from '../theme/themeprovider';
 import Button from './Button';
 import withTheme from '../theme/withTheme';
 
 class Main extends Component {
-
-  // commenting this coz this was the lazy approach :D
-  // for references, so no need to remove
-  // render() {
-  //   return (
-  //     <View style={this.context.theme.rootView}>
-  //       <Text style={this.context.theme.label}>Hello World</Text>
-  //       <Button onPress={this.props.onChangeTheme} label='Change Theme' />
-  //     </View>
-  //   );
-  // }
 
   render() {
     return (
@@ -28,24 +16,5 @@ class Main extends Component {
   }
 }
 
-// not so lazy approach and not polluting context props
-export default withTheme({
-  theme: {
-    rootView: {
-      flex: PropTypes.number,
-      backgroundColor: PropTypes.string,
-      flexDirection: PropTypes.string,
-      justifyContent: PropTypes.string,
-      alignItems: PropTypes.string,
-    },
-    label: {
-      color: PropTypes.string,
-      fontSize: PropTypes.number,
-      fontStyle: PropTypes.string,
-    },
-  }
-})(Main)
-
-// commenting this coz this was the lazy approach :D
-//Main.contextTypes = ThemeProvider.childContextTypes;
+export default withTheme(Main)
 
--- src/theme/themeprovider.js
+++ src/theme/themeprovider.js
@@ -1,48 +1,17 @@
 import React, { Component, Children } from 'react';
 import PropTypes from 'prop-types';
 
-export default class ThemeProvider extends Component {
+export default class ThemeProvider extends React.Component {
+
+  static childContextTypes = {
+    theme: PropTypes.object
+  };
+
   getChildContext() {
-    return {
-      theme: this.props.theme,
-    };
+    return { theme: this.props.theme };
   }
+
   render() {
-    return Children.only(this.props.children);
+    return this.props.children;
   }
 }
-
-// temporary, this will be replaced with HOC soon
-ThemeProvider.childContextTypes = {
-  theme: PropTypes.object
-}
-
-// ThemeProvider.childContextTypes = {
-//   theme: PropTypes.shape({
-//     rootView: {
-//       flex: PropTypes.number,
-//       backgroundColor: PropTypes.string,
-//       flexDirection: PropTypes.string,
-//       justifyContent: PropTypes.string,
-//       alignItems: PropTypes.string,
-//     },
-//     button: {
-//       borderRadius: PropTypes.number,
-//       padding: PropTypes.number,
-//       backgroundColor: PropTypes.string,
-//       margin: PropTypes.number,
-//     },
-//     buttonLabel: {
-//       color: PropTypes.string,
-//       fontStyle: PropTypes.string,
-//     },
-//     label: {
-//       color: PropTypes.string,
-//       fontSize: PropTypes.number,
-//       fontStyle: PropTypes.string,
-//     },
-//     extras: {
-//       underlayColor: PropTypes.string,
-//     }
-//   }),
-// };
