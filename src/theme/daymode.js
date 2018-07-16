b90f33cddaa4d95c370a7aabf68a5457d04380c1
xtrycatchx <xtrycatchx@gmail.com>
2018-07-16 23:55:35 +0800

    clean up

--- src/theme/daymode.js
+++ src/theme/daymode.js
@@ -1,7 +1,7 @@
 export default {
   rootView: {
     flex: 1,
-    backgroundColor: "#0D73E0",
+    backgroundColor: '#0D73E0',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
@@ -9,19 +9,19 @@ export default {
   button: {
     borderRadius: 0,
     padding: 14,
-    backgroundColor: "#E0C22D",
-    margin: 20
+    backgroundColor: '#E0C22D',
+    margin: 20,
   },
   buttonLabel: {
-    color: "#0D73E0",
-    fontStyle: "italic"
+    color: '#0D73E0',
+    fontStyle: 'italic',
   },
   label: {
-    color: "#E0C22D",
+    color: '#E0C22D',
     fontSize: 20,
-    fontStyle: "italic"
+    fontStyle: 'italic',
   },
   extras: {
     underlayColor: '#0D73E0',
-  }
-}
+  },
+};
--- src/theme/nightmode.js
+++ src/theme/nightmode.js
@@ -1,7 +1,7 @@
 export default {
   rootView: {
     flex: 1,
-    backgroundColor: "#141513",
+    backgroundColor: '#141513',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
@@ -9,19 +9,19 @@ export default {
   button: {
     borderRadius: 20,
     padding: 14,
-    backgroundColor: "#638149",
-    margin: 20
+    backgroundColor: '#638149',
+    margin: 20,
   },
   buttonLabel: {
-    color: "#141513",
-    fontStyle: "normal"
+    color: '#141513',
+    fontStyle: 'normal',
   },
   label: {
-    color: "#638149",
+    color: '#638149',
     fontSize: 20,
-    fontStyle: "normal"
+    fontStyle: 'normal',
   },
   extras: {
     underlayColor: '#555539',
-  }
-}
+  },
+};
