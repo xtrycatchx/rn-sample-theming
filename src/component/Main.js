9b910189b6ad75d2bdaab82e84c8e00b9fcf9069
xtrycatchx <xtrycatchx@gmail.com>
2018-07-16 23:49:47 +0800

    clean up

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,17 +1,27 @@
 import React, { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import withTheme from '../theme/withTheme';
 
 class Main extends PureComponent {
   render() {
+    const { onChangeTheme, theme } = this.props;
+    const { rootView, label } = theme;
     return (
-      <View style={this.props.theme.rootView}>
-        <Text style={this.props.theme.label}>Hello World</Text>
-        <Button onPress={this.props.onChangeTheme} label="Change Theme" />
+      <View style={rootView}>
+        <Text style={label}>
+        Hello World
+        </Text>
+        <Button onPress={onChangeTheme} label="Change Theme" />
       </View>
     );
   }
 }
+
+Main.propTypes = {
+  theme: ViewPropTypes.style.isRequired,
+  onChangeTheme: PropTypes.func.isRequired,
+};
 
 export default withTheme(Main);
