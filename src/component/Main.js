4391a68a2f9906c6c16317e717d85506606bcddf
xtrycatchx <xtrycatchx@gmail.com>
2018-07-16 23:40:02 +0800

    clean up

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native';
 import Button from './Button';
 import withTheme from '../theme/withTheme';
 
-class Main extends Component {
-
+class Main extends PureComponent {
   render() {
     return (
       <View style={this.props.theme.rootView}>
         <Text style={this.props.theme.label}>Hello World</Text>
-        <Button onPress={this.props.onChangeTheme} label='Change Theme' />
+        <Button onPress={this.props.onChangeTheme} label="Change Theme" />
       </View>
     );
   }
 }
 
-export default withTheme(Main)
-
+export default withTheme(Main);
