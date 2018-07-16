32e1e27591cc52c2dcb587778c3870fd4f46dcea
xtrycatchx <xtrycatchx@gmail.com>
2018-07-16 23:54:11 +0800

    clean up

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,19 +1,30 @@
 import React, { PureComponent } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
 import withTheme from '../theme/withTheme';
 
 class Button extends PureComponent {
   render() {
+    const { theme, onPress, label } = this.props;
     return (
       <TouchableOpacity
         activeOpacity={60}
-        underlayColor={this.props.theme.extras.underlayColor}
-        onPress={this.props.onPress}
-        style={this.props.theme.button}>
-        <Text style={this.props.theme.buttonLabel}>{this.props.label}</Text>
+        underlayColor={theme.extras.underlayColor}
+        onPress={onPress}
+        style={theme.button}
+      >
+        <Text style={theme.buttonLabel}>
+          {label}
+        </Text>
       </TouchableOpacity>
     );
   }
 }
+
+Button.propTypes = {
+  theme: ViewPropTypes.style.isRequired,
+  onPress: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
 
 export default withTheme(Button);
